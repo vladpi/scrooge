dadcee81736051cb85122ce0dc28933268e17c4c
Vlad Pastushenko <iam@vladpi.me>
2024-11-14 23:37:00 +0300

    clean up repo

--- data/sql/services.py
+++
@@ -1,9 +0,0 @@
-from rodi import Container
-
-from domain.users import UsersRepository
-
-from .users import SQLUsersRepository
-
-
-def register_sql_services(container: Container) -> None:
-    container.add_scoped(UsersRepository, SQLUsersRepository)
--- domain/services.py
+++
@@ -1,9 +0,0 @@
-from rodi import Container
-
-from .passwords import PasswordsService
-from .users import UsersService
-
-
-def register_services(container: Container) -> None:
-    container.add_scoped(UsersService)
-    container.add_scoped(PasswordsService)
